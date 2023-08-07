0061fb7661c2c9644e846704ef37f6b2c2bb28b5
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-08-07 11:45:35 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,23 @@
+class prime {
+    public static void main(String[] args){
+         
+        int flag=0;
+          System.out.println( "prime num");
+         for(int numm=3; numm<=100; numm++){
+             flag=0;
+             for(int j=2; j<=numm/2; j++){
+                 if(numm%j==0){
+                    flag=1; 
+                    break;
+                 }
+             }
+             if(flag==0){
+                 System.out.println( numm );
+                 flag=0;
+             }
+             
+         }
+    
+     }
+   
+ }
