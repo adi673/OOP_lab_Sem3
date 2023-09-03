5afe7f3b9da138b0144becb8cdae6fc93ae74a5d
Aditya Wadgaonkar <119002724+adi673@users.noreply.github.com>
2023-09-03 10:42:02 +0530

    Update Q5]Search.java

--- Lab3/Q5]Search.java
+++ Lab3/Q5]Search.java
@@ -5,37 +5,31 @@ import java.io.*;
 import java.util.Scanner;
 
 
-class Easy
+class Easy{
+	public static void main(String[] args){
 
-{
+        	Scanner scanner = new Scanner(System.in);
+        	// int arr;
+        	int size,key;
+        	System.out.print("Enter the size of arrray ");
+        	size = scanner.nextInt();
 
-	public static void main(String[] args)
-
-	{
-
-        Scanner scanner = new Scanner(System.in);
-        // int arr;
-        int size,key;
-        System.out.print("Enter the size of arrray ");
-        size = scanner.nextInt();
-
-        System.out.print("Enter the key to find ");
-        key = scanner.nextInt();
-        int arr[] = new int[size]; 
+        	System.out.print("Enter the key to find ");
+        	key = scanner.nextInt();
+        	int arr[] = new int[size]; 
 		// array declaration
 
 		System.out.println("Enter the elements of the array: ");  
-       for(int i=0; i<size; i++)  {  
-            //reading array elements from the user   
-            arr[i]=scanner.nextInt();  
-        }  
-        int i=1;
-		for ( int var : arr){
-            
-            if(var==key){
-			    System.out.print("key found at position "+ i );
-            }
-            i++;
-        }
+       		for(int i=0; i<size; i++)  {  
+            		//reading array elements from the user   
+           		arr[i]=scanner.nextInt();  
+        	}  
+        	int i=1;
+		for ( int var : arr){   
+            		if(var==key){
+				System.out.print("key found at position "+ i );
+            		}
+            		i++;
+        	}
 	}
 }
